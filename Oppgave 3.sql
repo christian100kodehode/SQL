060049395c0417e3ce0c281ef1ad6f67b05b4593
christian100.kodehode <christian100.kodehode@gmail.com>
2025-11-01 13:39:21 +0100

    Task 3 continue

--- Oppgave 3.sql
+++ Oppgave 3.sql
@@ -16,4 +16,23 @@ use ga_bibliotek;
 SELECT Tittel, AntallSider FROM bok
 WHERE AntallSider > 300;
 
-4.
+4. Legge til en ny bok i tabellen 'bok'
+INSERT INTO bok (ISBN, Tittel, Forfatter, Forlag, UtgittÅr, AntallSider)
+VALUES 
+("4563469687","Amiga: Back Again","Joe, Bill","Boing",2020,188)
+
+5. Legge til en ny låner i tabellen 'låner'
+
+6. Oppdatere adresse for en spesifikk låner
+
+7. Hente alle utlån sammen med lånerens navn og bokens tittel
+
+8. Alle bøker og antall eksemplarer for hver bok
+
+9. Antall utlån per låner
+
+10. Antall utlån per bok
+
+11. Alle bøker som ikke har blitt lånt ut
+
+12. Forfatter og antall utlånte bøker per forfatter
